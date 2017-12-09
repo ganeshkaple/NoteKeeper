925a863a341ce1e5922f69c5763da64c31f5f1f7
Ganesh Kaple <gvklive@gmail.com>
2017-12-09 17:16:01 +0530

    Implemented User Interaction with Listview, Added Listview

---
+++ app/src/main/java/com/test/notekeeper/NoteListActivity.java
@@ -0,0 +1,40 @@
+package com.test.notekeeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class NoteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        initializeDisplayContent();
+    }
+
+    private void initializeDisplayContent() {
+        ListView listView = findViewById(R.id.list_note);
+        List<NoteInfo> notes =DataManager.getInstance().getNotes();
+
+        ArrayAdapter<NoteInfo> adapter = new ArrayAdapter<NoteInfo>(this,android.R.layout.simple_list_item_1,notes);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
