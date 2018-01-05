3c33316ed4c300e12786db8ec13bb7ecb19e9ec8
Ganesh Kaple <gvklive@gmail.com>
2018-01-05 23:33:35 +0530

    Unit tests everywhere

--- app/src/test/java/com/test/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/test/notekeeper/DataManagerTest.java
@@ -1,5 +1,7 @@
 package com.test.notekeeper;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
@@ -9,9 +11,20 @@ import static junit.framework.Assert.assertEquals;
  * @since 05-01-2018
  */
 public class DataManagerTest {
+    static DataManager dataManager;
+
+    @BeforeClass
+    public static void classSetUp() throws Exception {
+        dataManager = DataManager.getInstance();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        dataManager.getNotes().clear();
+        dataManager.initializeExampleNotes();
+    }
     @Test
     public void createNewNote() throws Exception {
-        DataManager dataManager = DataManager.getInstance();
         final CourseInfo courseInfo = dataManager.getCourse("android_async");
         final String noteTitle = "test note title";
         final String noteText = "test note text";
@@ -28,4 +41,32 @@ public class DataManagerTest {
         assertEquals(noteTitle, compareInfo.getTitle());
     }
 
+    @Test
+    public void findSimilarNotes() throws Exception {
+        final CourseInfo course = dataManager.getCourse("android_async");
+        final String noteTitle = "test note title";
+        final String noteText = "test note text";
+
+        int index1 = dataManager.createNewNote();
+        NoteInfo noteInfo1 = dataManager.getNotes().get(index1);
+        noteInfo1.setCourse(course);
+        noteInfo1.setText(noteText);
+        noteInfo1.setTitle(noteTitle);
+
+        /*int index2 = dataManager.createNewNote();
+        NoteInfo noteInfo2 = dataManager.getNotes().get(index2);
+        noteInfo2.setCourse(course);
+        noteInfo2.setText(noteText);
+        noteInfo2.setTitle(noteTitle);
+*/
+        int foundIndex1 = dataManager.findNote(noteInfo1);
+        assertEquals(index1, foundIndex1);
+
+
+        /*int foundIndex2 = dataManager.findNote(noteInfo2);
+        assertEquals(index2,foundIndex2);
+*/
+
+
+    }
 }
