a0bf7fda9e209e3a15abce3baca0c219307257ec
Ganesh Kaple <gvklive@gmail.com>
2018-01-06 21:45:55 +0530

    Instrumentation Test on Data, means on adapter, in this case spinner

--- app/src/androidTest/java/com/test/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/test/notekeeper/NoteCreationTest.java
@@ -2,15 +2,22 @@ package com.test.notekeeper;
 
 import android.support.test.rule.ActivityTestRule;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 
 /**
  * @author Ganesh Kaple
@@ -18,19 +25,31 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
  */
 @RunWith(JUnit4.class)
 public class NoteCreationTest {
+    static DataManager dataManager;
     @Rule
     public ActivityTestRule<NoteListActivity> noteListActivityActivityTestRule = new ActivityTestRule<>(NoteListActivity.class);
 
+    @BeforeClass
+    public static void setUp() throws Exception {
+        dataManager = DataManager.getInstance();
+    }
+
     @Test
     public void createNewNote() {
-
+        final CourseInfo course = dataManager.getCourse("java_lang");
         final String noteTitle = "Test Title";
         final String noteText = "Test Text dummy";
 
         onView(withId(R.id.fab)).perform(click());
 
+        onView(withId(R.id.spinner_courses)).perform(click());
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
+
         onView(withId(R.id.text_note_title)).perform(typeText(noteTitle));
         onView(withId(R.id.text_note_text)).perform(typeText(noteText));
+        closeSoftKeyboard();
+
+        pressBack();
 
     }
 }
