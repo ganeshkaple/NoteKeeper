46a606e51683f67bc1698bc1527e70c53e2985a8
Ganesh Kaple <gvklive@gmail.com>
2018-02-24 13:59:40 +0530

    Implemented Content Provider

--- app/src/main/java/com/test/notekeeper/NoteActivity.java
+++ app/src/main/java/com/test/notekeeper/NoteActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -358,7 +359,14 @@ public class NoteActivity extends AppCompatActivity
 
     private CursorLoader createLoaderCourses() {
         mCoursesQueryFinished = false;
-        return new CursorLoader(this) {
+
+        Uri uri = Uri.parse("content://com.test.notekeeper.provider");
+        String[] courseColumns = {
+                CourseInfoEntry.COLUMN_COURSE_TITLE,
+                CourseInfoEntry.COLUMN_COURSE_ID,
+                CourseInfoEntry._ID};
+        return new CursorLoader(this, uri, courseColumns, null, null, CourseInfoEntry.COLUMN_COURSE_TITLE);
+       /* return new CursorLoader(this) {
             @Override
             public Cursor loadInBackground() {
                 SQLiteDatabase db = mDbOpenHelper.getReadableDatabase();
@@ -370,7 +378,7 @@ public class NoteActivity extends AppCompatActivity
                 return db.query(CourseInfoEntry.TABLE_NAME, courseColumns,
                         null, null, null, null, CourseInfoEntry.COLUMN_COURSE_TITLE);
             }
-        };
+        };*/
     }
 
     private CursorLoader createLoaderNotes() {
